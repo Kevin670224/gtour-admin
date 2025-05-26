1f02abcc183a939dce0949b6fa88f9ff2e04884b
Kevin670224 <>
2025-05-26 12:12:43 +0700

    🧩 quote_bp 명시 export 추가

--- routes/admin_quote.py
+++ routes/admin_quote.py
@@ -1,9 +1,11 @@
 from flask import Blueprint, render_template
 from models import Quote
 
-admin_quote_bp = Blueprint("admin_quote", __name__, url_prefix="/admin/quotes")
+quote_bp = Blueprint("quote", __name__, url_prefix="/admin")
 
-@admin_quote_bp.route("/")
+@quote_bp.route("/quotes")
 def quote_list():
     quotes = Quote.query.order_by(Quote.created_at.desc()).all()
     return render_template("admin/quote_list.html", quotes=quotes)
+
+__all__ = ['quote_bp']
